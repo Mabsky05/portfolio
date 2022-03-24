ac0c625cf6bc2f0f849f4388bf016939cd088749
Mabsky <grimdango@gmail.com>
2022-03-24 21:07:55 +1100

    first upload

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+function contact() {
+    contact_info = document.querySelector(".contact")
+    contact_info.innerHTML = "grimdango@gmail.com &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 0421441463";
+    contact_info.style.fontSize=".8em"
+    contact_info.style.color="grey"
+ }
+
+ document.querySelector(".contact").addEventListener('click', contact);
+
+ function about() {
+    contact_info = document.getElementById("about")
+    var img = document.createElement('img')
+    img.src = "assets"+ "\\"+ "profpic2.jpg"
+    document.getElementById("my_name").innerHTML = "<img src=" + img.src + " width='100px'" + ">"
+    contact_info.innerHTML = "Software Developer <br> HTML CSS Javascript <br> Bootstrap Jquery <br> Web design and development";
+    contact_info.style.fontSize=".8em"
+    contact_info.style.color="grey"
+    contact_info.style.cursor="none"
+ }
+
+
+ function about_clear() {
+     document.getElementById("about").innerHTML = "About";
+     document.getElementById("my_name").innerHTML = "Miguel Afable"
+     contact_info.style.fontSize="1em"
+     contact_info.style.color="azure"
+ }
+ 
+ document.getElementById("about").addEventListener('mousedown', about);
+ document.getElementById("about").addEventListener('mouseup', about_clear);
+
+ 
---
+++ README.md
@@ -0,0 +1,31 @@
+# Profile Page
+
+![alt text](assets/sample.jpg)
+![alt text](assets/sample.gif)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Summary
+Profile page showcasing Github projects and professional credentials 
+
+## Description / Instructions
+Upon loading: Site will show top navbar and center carousel. 
+
+Navbar contents, left to right: Linked and Github links, About (click and display), Contact (click and reveal), drop for CV and folio.
+
+Carousel, each slide navigated by arrow: Github project (click on photo), repo link (click on link). 
+
+## Contact
+grimdango@gmail.com
+
+## Log 
+### 04/02/22
+Major upload.
+
+### 31/01/22
+Core website elements laid out.  
+
+### 29/01/22
+First commit. 
+   
+
